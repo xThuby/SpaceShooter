ef838b88c7bddce5d979669025a9e66d3963964a
Oliver A. Thunæs <oliver.thunaes@gmail.com>
2024-10-25 15:35:28 +0200

    Mouselook

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     [SerializeField]
+    private float mouseSensitivity = 1f;
+
+    [SerializeField]
     private Transform playerBody;
 
-    private float lookY;
+    private Vector3 lookRotation;
+    private Vector3 mouseDelta;
 
     private void Start()
     {
@@ -14,8 +18,20 @@ public class MouseLook : MonoBehaviour
 
     private void Update()
     {
-        lookY += Input.GetAxis("Mouse X");
+        UpdateInput();
 
-        playerBody.rotation = Quaternion.Euler(0, lookY, 0);
+        lookRotation.x -= mouseDelta.y * mouseSensitivity;
+        lookRotation.y += mouseDelta.x * mouseSensitivity;
+        lookRotation.x = Mathf.Clamp(lookRotation.x, -89f, 89f);
+
+        transform.localRotation = Quaternion.Euler(lookRotation.x, 0, 0);
+
+        Vector3 forward = Quaternion.AngleAxis(lookRotation.y, Vector3.up) * Vector3.forward;
+        playerBody.rotation = Quaternion.LookRotation(forward, Vector3.up);
+    }
+
+    private void UpdateInput()
+    {
+        mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
     }
 }
